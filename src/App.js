f8763b22fbaf45588a22211c2003aee127d02485
ozubergz <34900230+ozubergz@users.noreply.github.com>
2020-05-23 21:49:46 -0400

    update template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-// step 1: first create a Banner component
-// step 2: render Banncer component inside App Component
-// step 3: create handleClose function to close Modal when clicking close button
-// step 4: pass handleClose function to Modal component as props
-
 import React from "react";
 import "./styles.css";
 import CardContainer from "./containers/CardContainer";
-
 import Modal from "./components/Modal";
 import Banner from "./components/Banner";
 
